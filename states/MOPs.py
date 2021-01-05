4983a271643123813454f0496d0b5751f45cb89b
Олег Потребчук <potrebchuk0@gmail.com>
2021-01-05 23:14:28 +0300

    ThirdCommit

--- states/MOPs.py
+++ states/MOPs.py
@@ -3,3 +3,10 @@ from aiogram.dispatcher.filters.state import StatesGroup, State
 
 class MOP(StatesGroup):
     finishing_material = State()  # материал отделки
+    stairs_to_the_attic = State()  # лестница на чердак
+    description_of_steps = State()  # описание ступеней и их количество, размеры, сколько ступеней с дефектами
+    flooring = State()  # напольное покрытие, есть ли дефекты, отметить на плане и описать
+    material = State()  # материал
+    dimensions = State()  # размеры
+    shoe_box = State()  # есть ли галошница
+
